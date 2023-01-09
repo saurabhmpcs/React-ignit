7562d1a6a00815ab00569514aade19847e731c3f
Saurabh <saurabh.rgt@gmail.com>
2023-01-09 08:49:46 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement("h1", { id: "title" }, "Heading 1");
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+root.render(container);
